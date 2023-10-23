557745ffcf51cf9977d56e8e50394de54aef578b
srinadhmuppalla <39974927+srinadhmuppalla@users.noreply.github.com>
2023-10-23 12:24:43 -0400

    Update add.java

--- add.java
+++ add.java
@@ -1,88 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-class Expense {
-    private String description;
-    private double amount;
-
-    public Expense(String description, double amount) {
-        this.description = description;
-        this.amount = amount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-}
-
-public class ExpenseTracker {
-    private List<Expense> expenses;
-
-    public ExpenseTracker() {
-        expenses = new ArrayList<>();
-    }
-
-    public void addExpense(String description, double amount) {
-        Expense expense = new Expense(description, amount);
-        expenses.add(expense);
-    }
-
-    public double calculateTotalExpenses() {
-        double total = 0;
-        for (Expense expense : expenses) {
-            total += expense.getAmount();
-        }
-        return total;
-    }
-
-    public void displayExpenses() {
-        System.out.println("Expense List:");
-        for (Expense expense : expenses) {
-            System.out.println("Description: " + expense.getDescription() + ", Amount: $" + expense.getAmount());
-        }
-    }
-
+public class SumCalculator {
     public static void main(String[] args) {
-        ExpenseTracker tracker = new ExpenseTracker();
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("\nExpense Tracker Menu:");
-            System.out.println("1. Add Expense");
-            System.out.println("2. View Expenses");
-            System.out.println("3. Calculate Total Expenses");
-            System.out.println("4. Exit");
-            System.out.print("Enter your choice: ");
-            
-            int choice = scanner.nextInt();
-            scanner.nextLine();  // Consume the newline character
+        System.out.println("Enter the first number: ");
+        int num1 = scanner.nextInt();
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter expense description: ");
-                    String description = scanner.nextLine();
-                    System.out.print("Enter expense amount: $");
-                    double amount = scanner.nextDouble();
-                    tracker.addExpense(description, amount);
-                    break;
-                case 2:
-                    tracker.displayExpenses();
-                    break;
-                case 3:
-                    double total = tracker.calculateTotalExpenses();
-                    System.out.println("Total Expenses: $" + total);
-                    break;
-                case 4:
-                    System.out.println("Goodbye!");
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
+        System.out.println("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        scanner.close();
     }
 }
